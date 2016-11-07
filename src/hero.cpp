c5db0d357f2bf65b904c4c8d25e4da47de233b3e
Prichman <prichmen@gmail.com>
2016-11-07 16:25:35 +0700

    Added functionality to Hero class

--- src/hero.cpp
+++ src/hero.cpp
@@ -1,15 +1,35 @@
 #include "hero.h"
 
-Hero::Hero() {
+#include <string>
 
+static const int livesCount = 3;
+static const int restoreTime = 500;
+static const int dt = 16;
+
+Hero::Hero():
+ lives_count_(livesCount),
+ score_(0),
+ restore_(false) {
+   LoadSprite("resources/hero.png");
+}
+
+void Hero::GameUpdate() {
+  if (restore_) {
+    // Restore the hero.
+    restore_time_ -= dt;
+
+    // TODO: change animation.
+
+    if (restore_time_ <= 0) {
+      restore_ = false;
+    }
+  }
+  
+  AttackActor::GameUpdate();
 }
 
 void Hero::AddLife() {
   ++lives_count_;
-}
-
-void Hero::SubLife() {
-  --lives_count_;
 }
 
 sf::Uint32 Hero::GetLivesCount() const {
@@ -22,4 +42,28 @@ void Hero::AddScore(int score) {
 
 sf::Uint32 Hero::GetScore() const {
   return score_;
+}
+
+void Hero::Die() {
+  SubLife();
+  if (lives_count_ > 0)
+    Revive();
+  else {
+    // TODO: add ending.
+  }
+}
+
+bool Hero::isResurrecting() const {
+  return restore_;
+}
+
+
+
+void Hero::SubLife() {
+  --lives_count_;
+}
+
+void Hero::Revive() {
+  restore_time_ = restoreTime;
+  restore_ = true;
 }
--- src/hero.h
+++ src/hero.h
@@ -6,14 +6,26 @@ class Hero : public AttackActor {
  public:
   Hero();
 
+  void GameUpdate() override;
+
   void AddLife();
-  void SubLife();
   sf::Uint32 GetLivesCount() const;
 
   void AddScore(int score);
   sf::Uint32 GetScore() const;
 
+  void Die();
+  bool isResurrecting() const;
+
  private:
+  void SubLife();
+  void Revive();
+
   sf::Uint32 lives_count_;
   sf::Uint32 score_;
+
+  bool restore_;
+  int restore_time_;
+
+  // TODO: test changing color.
 };
